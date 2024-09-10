bd9ca451d69b0c145f94efdad0238ded32ea505a
Ujjval Joshi <ujjvaljoshi45@gmail.com>
2024-09-10 14:39:22 +0530

    paid status update done

--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -1,35 +1,36 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:contri_buter/models/user.dart';
 
+
 class TransactionModel {
   final String id;
   final String title;
   final String category;
-  final Map<String, dynamic> contributors;
+  final Map<String, dynamic> members;
   final DateTime dateTime;
   final double amount;
-  final Map<String, dynamic> unpaidParticipants;
-  final String createdBy;
+  // final Map<String, dynamic> unpaidParticipants;
+  final UserModel createdBy;
 
   TransactionModel({
     required this.id,
     required this.title,
     required this.category,
-    required this.contributors,
+    required this.members,
     required this.dateTime,
     required this.amount,
-    required this.unpaidParticipants,
+    // required this.unpaidParticipants,
     required this.createdBy,
   });
 
   toJson() => {
         'title': title,
         'category': category,
-        'contributors': contributors,
-        'unpaidParticipants': unpaidParticipants,
+        'members': members,
+        // 'unpaidParticipants': unpaidParticipants,
         'dateTime': dateTime,
         'amount': amount,
-        'createdBy': createdBy,
+        'createdBy': createdBy.toJson(),
       };
   // Factory method to create a Transaction from Firestore data
   factory TransactionModel.fromFirestore(DocumentSnapshot doc) {
@@ -38,25 +39,24 @@ class TransactionModel {
       id: doc.id,
       title: data['title'] ?? 'Unknown Title',
       category: data['category'] ?? 'Unknown Category',
-      contributors: data['contributors'],
-      unpaidParticipants: data['unpaidParticipants'],
+      members: data['members'],
+      // unpaidParticipants: data['unpaidParticipants'],
       dateTime: (data['dateTime'] as Timestamp).toDate(),
       amount: data['amount'].toDouble(),
-      createdBy: data['createdBy'],
+      createdBy: UserModel.fromJson(data['createdBy']),
     );
   }
   @override
   String toString() {
-    return 'TransactionModel(id: $id, category: $category, contributors: $contributors, dateTime: $dateTime, amount: $amount, unpaidParticipants: $unpaidParticipants, title: $title)';
+    return 'TransactionModel(id: $id, category: $category, contributors: $members, dateTime: $dateTime, amount: $amount, title: $title)';
   }
 
-  static Map<String, dynamic> peopleFromList(
-      List<UserModel> contacts, double amount) {
+  static Map<String, dynamic> peopleFromList(List<UserModel> contacts, double amount) {
     final Map<String, dynamic> res = {};
     for (UserModel contact in contacts) {
       res.putIfAbsent(
         contact.phoneNumber,
-        () => {'name': contact.userName, 'amount': amount, 'profile_image':contact.profileImage},
+        () => {'name': contact.userName, 'amount': amount, 'profile_image': contact.profileImage},
       );
     }
     return res;
